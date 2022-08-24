50ba1b0e59dbe1faca03557a4cdb9e581e468823
Mayara Lima <91838870+mayalimac@users.noreply.github.com>
2022-08-24 11:28:09 -0300

    Add files via upload

---
+++ manipulacao.string.java
@@ -0,0 +1,39 @@
+//manipulacao.strings
+
+
+//codigo 1
+public class ManipilacaoString { 
+
+    public class static void main(String[] args) {
+        String x = "Hi there!"; 
+        System.out.print(x.length()); 
+        System.out.print(x+ "concatenada"); 
+
+    }
+}
+
+
+ //codigo 2       
+public class Filme{
+
+public class static void main(String{} args) { 
+    String movie = "Harry Potter e o prisioneiro de Azkaban"; 
+    System.out.print(x.contains("prisioneiro")); 
+    System.out.print(movie.lastIndexOf("Azkaban")); 
+    }
+}
+
+
+
+//codigo 3
+
+public static void lastIndexOf(){
+    string nome= "Harry Potter";
+    int indice = nome.indexOf ("  ");
+    System.out.print("O espaço aparece primeiro na posição" + indice);
+    }
+}
+
+
+
+
